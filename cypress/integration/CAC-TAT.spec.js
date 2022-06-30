42b71592bca6f513a555ddf92b1678365af090e8
Adriano <adrianoaluizi@hotmail.com>
2022-06-30 17:24:15 -0300

    Suites de teste basica interagindo com form

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,93 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function () {
-    it('verifica o título da aplicação', function () {
+
+    beforeEach(() => {
         cy.visit('./src/index.html')
+    })
+
+    it('verifica o título da aplicação', () => {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
+
+    it('preenche os campos obrigatórios e envia o formulário', () => {
+
+        const longText = "teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste teste"
+
+        cy.get('#firstName').type('Adriano')
+        cy.get('#lastName').type('Aluizi')
+        cy.get('#email').type('adriano@email.com')
+        cy.get('#open-text-area').type(longText, { delay: 0 })
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.success').should('be.visible')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+        cy.get('#firstName').type('Adriano')
+        cy.get('#lastName').type('Aluizi')
+        cy.get('#email').type('adriano@email,com')
+        cy.get('#open-text-area').type('teste')
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+
+    })
+
+    it('campo telefone continua vazio quando preenchido com valor não numérico', () => {
+        cy.get('#phone')
+            .type('abcdefgh')
+            .should('have.value', '')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+        cy.get('#firstName').type('Adriano')
+        cy.get('#lastName').type('Aluizi')
+        cy.get('#email').type('adriano@email.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('teste')
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+        cy.get('#firstName')
+            .type('Adriano')
+            .should('have.value', 'Adriano')
+            .clear()
+            .should('have.value', '')
+        cy.get('#lastName')
+            .type('Aluizi')
+            .should('have.value', 'Aluizi')
+            .clear()
+            .should('have.value', '')
+        cy.get('#email')
+            .type('adriano@email.com')
+            .should('have.value', 'adriano@email.com')
+            .clear()
+            .should('have.value', '')
+        cy.get('#phone')
+            .type('1235463782')
+            .should('have.value', '1235463782')
+            .clear()
+            .should('have.value', '')
+
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('envia o formuário com sucesso usando um comando customizado', () => {
+        cy.fillMandatoryFieldsAndSubmit()
+
+        cy.get('.success').should('be.visible')
+    })
+
 })
