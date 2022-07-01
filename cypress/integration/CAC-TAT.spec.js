11b13ed853e2ba815f07775d0eeed8d5dc919fed
Adriano <adrianoaluizi@hotmail.com>
2022-07-01 17:57:16 -0300

    Desafio para encontrar o gato

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -233,10 +233,20 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     it('faz uma requisição HTTP', () => {
         cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
             .should((response) => {
-                const {status, statusText, body} = response
+                const { status, statusText, body } = response
                 expect(status).to.equal(200)
                 expect(statusText).to.equal('OK')
                 expect(body).to.include('CAC TAT')
-            })  
+            })
+    })
+
+    it('encontre o gato escondido', () => {
+        cy.get('#cat')
+            .invoke('show')
+            .should('be.visible')
+        cy.get('#title')
+            .invoke('text', 'CAT TAT')
+        cy.get('#subtitle')
+            .invoke('text', 'Eu ❤️ gatos')
     })
 })
